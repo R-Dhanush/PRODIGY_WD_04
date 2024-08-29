64512094f8120e1ec52075b20d7a80a4d88eb8d4
R-Dhanush <dhanu72004@gmail.com>
2024-08-29 22:56:37 +0530

    About Me

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ document.addEventListener('DOMContentLoaded', () => {
         showCursor: false
     });
 });
+
+var tablinks = document.getElementsByClassName("tab_link");
+var tabcontents = document.getElementsByClassName("tab_content");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active");
+    }
+
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active_content");
+    }
+    event.currentTarget.classList.add("active");
+    document.getElementById(tabname).classList.add("active_content");
+}
