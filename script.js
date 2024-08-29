bcc3cc9d21a8e9681f8452ed339492ff0340db49
R-Dhanush <dhanu72004@gmail.com>
2024-08-29 15:12:16 +0530

    header

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    var typed = new Typed('#animated-text',{
+        strings: ["Aspiring Full Stack Developer.", "I create interactive websites."],
+        typeSpeed: 60,
+        backSpeed: 60,
+        loop: true,
+        showCursor: false
+    });
+});
